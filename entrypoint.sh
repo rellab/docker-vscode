20df22f27e14553a6d63d4b613ab8cfe7d53a152
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2021-10-05 00:04:16 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ mkdir -p $VSCODE_HOME
 chown $VSCODE_USER:$VSCODE_GROUP $VSCODE_HOME
 su - $VSCODE_USER -c "cp -n -r --preserve=mode /etc/skel/. $VSCODE_HOME"
 
-su - $VSCODE_USER -c "cd $VSCODE_HOME && PASSWORD=$VSCODE_PASSWORD /opt/code-server/code-server --link --host 0.0.0.0 --port=$VSCODE_PORT --user-data-dir=$VSCODE_HOME/user_data"
+su - $VSCODE_USER -c "cd $VSCODE_HOME && PASSWORD=$VSCODE_PASSWORD /opt/code-server/code-server --host 0.0.0.0 --port=$VSCODE_PORT --user-data-dir=$VSCODE_HOME/user_data"
