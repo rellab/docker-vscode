51cbcbf781ef26891b2fd2857e6e88eb877efe3c
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2023-04-19 00:31:40 +0900

    update

--- Makefile
+++ Makefile
@@ -2,10 +2,11 @@
 
 IMAGE=rel/vscode
 
-build: build-cuda11
-
 build-cuda11: Dockerfile-cuda11
 	docker build -t ${IMAGE}:cuda11 -f Dockerfile-cuda11 .
+
+build-cuda10: Dockerfile-cuda10
+	docker build -t ${IMAGE}:cuda10 -f Dockerfile-cuda10 .
 
 clean:
 	docker images | grep ${IMAGE} | awk '{print $$3}' | xargs docker rmi
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+groupadd -f -g $VSCODE_GID $VSCODE_GROUP
+useradd -d $VSCODE_HOME -u $VSCODE_UID -g $VSCODE_GID -s /bin/bash $VSCODE_USER
+usermod -a -G dialout $VSCODE_USER
+
 echo "$VSCODE_USER:$VSCODE_PASSWORD" | chpasswd
 
 if [ $VSCODE_GRANT_SUDO = "yes" ]; then
@@ -12,4 +16,5 @@ mkdir -p $VSCODE_HOME
 chown $VSCODE_USER:$VSCODE_GROUP $VSCODE_HOME
 su - $VSCODE_USER -c "cp -n -r --preserve=mode /etc/skel/. $VSCODE_HOME"
 
-su - $VSCODE_USER -c "cd $VSCODE_HOME && PASSWORD=$VSCODE_PASSWORD /opt/code-server/code-server --host 0.0.0.0 --port=$VSCODE_PORT --user-data-dir=$VSCODE_HOME/user_data"
+su - $VSCODE_USER -c "cd $VSCODE_HOME && PASSWORD=$VSCODE_PASSWORD code-server --host 0.0.0.0 --port=$VSCODE_PORT"
+
